4307619462fa08b2a1ef19a34842a9ad3f974b64
Anthony LOUP <anthony.loup@insa-lyon.fr>
2018-04-25 13:10:15 +0200

    Cluster Fix
    Cluster if at least 2 items (instead of 5 minimum)

--- app/src/main/java/com/lugdunum/heptartuflette/lugdunum/Utils/Map/CustomClusterRenderer.java
+++ app/src/main/java/com/lugdunum/heptartuflette/lugdunum/Utils/Map/CustomClusterRenderer.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.maps.android.clustering.Cluster;
 import com.google.maps.android.clustering.ClusterManager;
 import com.google.maps.android.clustering.view.DefaultClusterRenderer;
 import com.lugdunum.heptartuflette.lugdunum.R;
@@ -23,5 +24,11 @@ public class CustomClusterRenderer extends DefaultClusterRenderer<ClusterItemPic
     @Override protected void onBeforeClusterItemRendered(ClusterItemPic item,
                                                          MarkerOptions markerOptions) {
         markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_pic_perso));
+    }
+
+    @Override
+    protected boolean shouldRenderAsCluster(Cluster<ClusterItemPic> cluster) {
+        //start clustering if at least 2 items overlap
+        return cluster.getSize() > 1;
     }
 }
