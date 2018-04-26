44a5d41d1ac11bc4a4704ee304749b93a90f9e82
Anthony LOUP <anthony.loup@insa-lyon.fr>
2018-04-26 11:22:28 +0200

    Implement camera

--- app/src/main/java/com/lugdunum/heptartuflette/lugdunum/Activity/ShowOldPhoto.java
+++ app/src/main/java/com/lugdunum/heptartuflette/lugdunum/Activity/ShowOldPhoto.java
@@ -1,10 +1,17 @@
 package com.lugdunum.heptartuflette.lugdunum.Activity;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -29,8 +36,77 @@ public class ShowOldPhoto extends AppCompatActivity {
             }
         });
     }
-    private void takePhoto() {
-        Intent myIntent = new Intent(this, TakePhoto.class);
-        startActivity(myIntent);
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_IMAGE_CAPTURE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission granted !
+                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED) {
+                        dispatchTakePictureIntent();
+                    }
+
+                } else {
+                    // permission denied
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setMessage(R.string.permission_message).setTitle(R.string.permission_message_title);
+                    builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener(){
+                        @Override
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.dismiss();
+                        }
+                    });
+
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+                return;
+            }
+        }
     }
+
+    private void takePhoto() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_GRANTED) {
+            // Already get permission
+            dispatchTakePictureIntent();
+        } else {
+            // No permission yet, asking through onRequestPermissionsResult
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.CAMERA)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CAMERA},
+                        REQUEST_IMAGE_CAPTURE);
+
+            }
+
+        }
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            //TODO : pass image in TakePhoto activity
+            Intent myIntent = new Intent(this, TakePhoto.class);
+            startActivity(myIntent);
+//            Bundle extras = data.getExtras();
+//            Bitmap imageBitmap = (Bitmap) extras.get("data");
+//            mImageView.setImageBitmap(imageBitmap);
+        }
+    }
+
 }
