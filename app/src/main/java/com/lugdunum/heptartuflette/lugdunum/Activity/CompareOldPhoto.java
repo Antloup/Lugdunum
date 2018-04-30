9cc95f267498e8c9c9f470b596714beb64c2bb1a
TheBastien <guiraudou.bastien@yahoo.fr>
2018-04-30 09:52:09 +0200

    Rating : récupération de la note

--- app/src/main/java/com/lugdunum/heptartuflette/lugdunum/Activity/CompareOldPhoto.java
+++ app/src/main/java/com/lugdunum/heptartuflette/lugdunum/Activity/CompareOldPhoto.java
@@ -3,11 +3,23 @@ package com.lugdunum.heptartuflette.lugdunum.Activity;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 
 import com.lugdunum.heptartuflette.lugdunum.R;
+import com.lugdunum.heptartuflette.lugdunum.Utils.JsonUtils;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
 
 public class CompareOldPhoto extends AppCompatActivity {
 
@@ -19,6 +31,22 @@ public class CompareOldPhoto extends AppCompatActivity {
         setContentView(R.layout.activity_compare_old_photo);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        Button buttonVote = (Button) findViewById(R.id.buttonVoter);
+        buttonVote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RatingBar voteBar = (RatingBar) findViewById(R.id.ratingPhoto);
+                float rating = voteBar.getRating();
+                String string1 = "Rating = " + rating;
+                Snackbar mySnackbar = Snackbar.make(view,
+                        string1, Snackbar.LENGTH_SHORT);
+                mySnackbar.show();
+
+            //TODO : Envoyer la note sur le serveur
+            }
+        });
 
 
         //Fill up view
